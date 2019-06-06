cc8698318e02596b9e3467e2698de73ff437fd7f
Sourav Pati <spati@echoenergy.com>
2019-06-06 11:04:10 -0500

    added tweet data

---
+++ src/main/java/com/okc/jug/kafka/consumer/demo/consumerdemo/config/ElasticsearchConfig.java
@@ -0,0 +1,47 @@
+package com.okc.jug.kafka.consumer.demo.consumerdemo.config;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.host-scheme}")
+    private String hostScheme;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
+    @Bean(destroyMethod = "close")
+    public RestHighLevelClient client() {
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder restClientBuilder = RestClient
+                .builder(new HttpHost(host, port, hostScheme))
+                .setHttpClientConfigCallback(builder -> builder.setDefaultCredentialsProvider(credentialsProvider));
+
+        return new RestHighLevelClient(restClientBuilder);
+    }
+
+
+}
--- src/main/java/com/okc/jug/kafka/consumer/demo/consumerdemo/service/TweetConsumerService.java
+++ src/main/java/com/okc/jug/kafka/consumer/demo/consumerdemo/service/TweetConsumerService.java
@@ -3,10 +3,16 @@ package com.okc.jug.kafka.consumer.demo.consumerdemo.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
 @Service
@@ -16,12 +22,33 @@ public class TweetConsumerService {
 
     private CountDownLatch latch = new CountDownLatch(1);
 
+    private  final RestHighLevelClient client;
+
     @KafkaListener(topics = "twitter_tweet",
             containerFactory = "kafkaListenerContainerFactory")
     public void consumeAsString(ConsumerRecord<String, String> cr,
                                 @Payload String payload) {
         log.info(payload);
+        sendTweetToEs(payload);
+
         latch.countDown();
+    }
+
+    private void sendTweetToEs(String tweet){
+
+        try {
+
+            IndexRequest request = new IndexRequest("tweet","_doc",UUID.randomUUID().toString())
+                    .source(tweet,XContentType.JSON);
+
+            IndexResponse response = client.index(request,RequestOptions.DEFAULT);
+
+
+        }catch (Exception e){
+
+                log.error(e.getMessage());
+        }
+
     }
 
 }
